618838186da1b25ba1681ef78f64155bc5ecd6ee
Odair José Custodio Junior <odair.dev@gmail.com>
2022-07-06 14:17:47 -0300

    typeorm configured

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import "reflect-metadata";
+import getConnection from "../typeorm/index"
 import { routes } from './routes'
+
+getConnection()
 
 const app = express()
 const port = 3333
